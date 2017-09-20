703516a95dde5ccfb2a217b149886fe5e65d6a6b
Eric Abell <ericabell@users.noreply.github.com>
2017-09-20 10:18:33 -0500

    Compute running time of fibo sequence

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
 
+    public static long calcFibonacci (int index) {
+        if (index == 1) {
+            return 1;
+        }
+        if (index == 0) {
+            return 0;
+        }
+        return calcFibonacci(index - 1) + calcFibonacci(index - 2);
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 40; i++) {
+            System.out.println("calc(" + i + ") = " + calcFibonacci(i));
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Execution time: " + (endTime - startTime));
     }
 }
